ce7cc6cd7eef6cf77bc66775c9fac859def0823c
Rudolfo Blake <69606511+rudolfoblake@users.noreply.github.com>
2026-08-08 08:22:58 -0300

    chore: bump Android app to 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.blakelabs.guitartuner"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
