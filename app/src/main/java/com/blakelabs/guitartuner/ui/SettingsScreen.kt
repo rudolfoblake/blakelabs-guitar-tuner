7ae4b1b5029259b07f071adca411c9fb3e6020c8
Rudolfo Blake <69606511+rudolfoblake@users.noreply.github.com>
2026-08-08 06:54:13 -0300

    fix: use scoped Compose weight modifier

--- app/src/main/java/com/blakelabs/guitartuner/ui/SettingsScreen.kt
+++ app/src/main/java/com/blakelabs/guitartuner/ui/SettingsScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
