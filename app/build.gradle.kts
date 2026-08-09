9e9119eac74490d9384463e41cb431e75697aa8d
Rudolfo Blake <69606511+rudolfoblake@users.noreply.github.com>
2026-08-09 09:33:46 -0300

    chore: distinguish rollback build as 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.blakelabs.guitartuner"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
