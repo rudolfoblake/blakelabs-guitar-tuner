7793e6d6936ca75a4f9f960bdb5d99cbc6d621fc
Rudolfo Blake <69606511+rudolfoblake@users.noreply.github.com>
2026-08-14 19:11:45 -0300

    Add iOS app entry point

---
+++ iosApp/BlakeTuner/BlakeTunerApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct BlakeTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
