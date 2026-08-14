3628bac5bb4bd055224bac7f13b3160768b3fc1a
Rudolfo Blake <69606511+rudolfoblake@users.noreply.github.com>
2026-08-14 19:10:13 -0300

    Add shared KMP build configuration

---
+++ shared/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+    id("com.android.kotlin.multiplatform.library")
+}
+
+kotlin {
+    android {
+        namespace = "com.blakelabs.guitartuner.shared"
+        compileSdk = 37
+        minSdk = 26
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    val xcf = XCFramework("BlakeTunerShared")
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "BlakeTunerShared"
+            isStatic = true
+            binaryOption("bundleId", "com.blakelabs.guitartuner.shared")
+            xcf.add(this)
+        }
+    }
+
+    sourceSets {
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
